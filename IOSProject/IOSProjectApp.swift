2c79a4f7f7e744c7cbbe2f5307efde2b2f7c7bc8
Erick Mendoza <erick_tec@outlook.com>
2022-05-01 14:49:57 -0500

    first commit

---
+++ IOSProject/IOSProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSProjectApp.swift
+//  IOSProject
+//
+//  Created by Erick Mendoza on 01/05/22.
+//
+
+import SwiftUI
+
+@main
+struct IOSProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# This is app for the final project of IOS
