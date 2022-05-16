896c04209289c1d8deadc7783d0b1fa253364162
Erick Mendoza <erick_tec@outlook.com>
2022-05-15 19:10:23 -0500

    firebase setup

--- IOSProject/IOSProjectApp.swift
+++ IOSProject/IOSProjectApp.swift
@@ -6,6 +6,17 @@
 //
 
 import SwiftUI
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+                   [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct IOSProjectApp: App {
